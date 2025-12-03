329825d072f7453a6747c8f36d2a5f940cc0f512
mmillns2 <matthewmillns@outlook.com>
2025-12-03 13:17:02 +0000

    fix cmd string name bugs

--- webserver/controller_server.py
+++ webserver/controller_server.py
@@ -13,29 +13,29 @@ class DeviceControllerServer:
         self.send_cmd(cmd_str)
 
     def turn_off_switch(self, device_name, channel):
-        cmd_str = f"turn_off_switch {device_name} {channel} {voltage}"
+        cmd_str = f"turn_off_switch {device_name} {channel}"
         self.send_cmd(cmd_str)
 
     # ---------------- Heater Functions ----------------
     def set_heater_temperature(self, device_name, channel, temperature):
-        cmd_str = f"set_heater_temperature {device_name} {channel} {voltage}"
+        cmd_str = f"set_heater_temperature {device_name} {channel} {temperature}"
         self.send_cmd(cmd_str)
 
     def turn_off_heater(self, device_name, channel):
-        cmd_str = f"turn_off_heater {device_name} {channel} {voltage}"
+        cmd_str = f"turn_off_heater {device_name} {channel}"
         self.send_cmd(cmd_str)
 
     def toggle_heater(self, device_name, channel, state: bool):
-        cmd_str = f"toggle_heater {device_name} {channel} {voltage}"
+        cmd_str = f"toggle_heater {device_name} {channel} {state}"
         self.send_cmd(cmd_str)
 
     # ---------------- Still Heater Functions ----------------
     def set_still_percentage(self, device_name, channel, percent):
-        cmd_str = f"set_still_percentage {device_name} {channel} {voltage}"
+        cmd_str = f"set_still_percentage {device_name} {channel} {percent}"
         self.send_cmd(cmd_str)
 
     def turn_off_still(self, device_name, channel):
-        cmd_str = f"turn_off_still {device_name} {channel} {voltage}"
+        cmd_str = f"turn_off_still {device_name} {channel}"
         self.send_cmd(cmd_str)
 
     # --------------- Remote Functions -----------------
